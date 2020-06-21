fd4d0ca4db98104f23a938462f2b14250f4ae646
fffffarmer <fffffarmer@sjtu.edu.cn>
2020-06-21 23:45:22 +0800

    clean up trash

--- graphdb/co_to_author.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-
-import_df = pd.read_csv('xxx.csv')
-unique_authors = pd.concat([import_df['Left'], import_df['Right']]).unique()
-print(unique_authors.shape)
-export_data = {'author_id': list(unique_authors)}
-export_df = pd.DataFrame(data=export_data)
-export_df.to_csv('US_author.csv', index=False)
